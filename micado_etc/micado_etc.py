cc8fe934f1c9a0e35baaa80676a229ca05f9008e
Miguel Verdugo <mverduol@gmail.com>
2021-10-15 17:10:09 +0200

    basic strucuture done

--- micado_etc/micado_etc.py
+++ micado_etc/micado_etc.py
@@ -5,7 +5,10 @@ from astropy.table import Table
 
 from spextra import Spextrum, Passband
 
+import scopesim as sim
+
 from .utils import *
+
 
 """
 Expected usage:
@@ -19,7 +22,10 @@ terget.sersic(parameters) # defines a spatial distribution  # default point sour
 target.get_source # get the source
 
 """
-
+sim.server.database.download_package(["locations/Armazones.zip",
+                                      "telescopes/ELT.zip",
+                                      "instruments/MAORY.zip",
+                                      "instruments/MICADO.zip"])
 
 SED_dict = dict(template=template,
                 MARCS=MARCS,
@@ -33,7 +39,6 @@ FLUX_DISTRO_dict = dict(sersic=sersic,
                         uniform=uniform_flux)
 
 
-
 class ETC:
 
     def __init__(self, mode="imaging", pixel_size=0.004, ao_mode="SCAO"):
@@ -41,36 +46,128 @@ class ETC:
         self.mode = mode
         self.pixel_size = pixel_size
         self.ao_mode = ao_mode
+        self.set_instrument(mode=self.mode, pixel_size=self.pixel_size, ao_mode=self.ao_mode)
 
+        # This could be used to read dictionaries directly from a yml file or a interactively
         self.spectrum = None
         self.source = None
         self.sky = None
+        self.setup = None
+        self.ao_params = None
 
-    def set_instrument(self):
+    def set_instrument(self, mode, pixel_size, ao_mode):
         """
         Reset instrument and perform checks
 
         """
-        pass
+        if mode.lower() not in ["spectroscopy", "imaging"]:
+            raise ValueError
+        if pixel_size not in [0.0015, 0.004]:
+            raise ValueError
+        if ao_mode.lower() not in ["scao", "mcao"]:
+            raise ValueError
+        if pixel_size == 0.015 and mode.lower() == "spectroscopy":
+            raise ValueError
+        if mode.lower() == "spectroscopy" and ao_mode.lower() == "mcao":
+            raise ValueError
+
+        self.mode = mode
+        self.pixel_size = pixel_size
+        self.ao_mode = ao_mode
+
 
     def set_sed(self, spectrum_type, **kwargs):
+        """
+
+        Parameters
+        ----------
+        spectrum_type:
+        kwargs
+
+        Returns
+        -------
+
+        """
+
+
         if spectrum_type not in SED_dict.keys():
             raise ValueError("SED not available")
 
-        self.spectrum = SED_dict[spectrum_type](**kwargs)
+        self.spectrum = dict(spec_name=spectrum_type, params=kwargs)
 
     def set_target_flux_distribution(self, distribution, **kwargs):
+        """
+        Same here
+        Parameters
+        ----------
+        distribution
+        kwargs
+
+        Returns
+        -------
+
+        """
         if distribution not in FLUX_DISTRO_dict.keys():
             raise ValueError("FLUX distribution not availalbe")
+        if self.spectrum is None:
+            raise ValueError("Please define the SED first")
 
-        self.source = FLUX_DISTRO_dict[distribution](self.spectrum, **kwargs)
+        self.source = dict(distro_name=distribution, params=kwargs)
 
-    def set_sky_conditions(self):
+    def _get_spectrum(self):
+        sed_func = SED_dict[self.spectrum["spec_name"]]
+        params = self.spectrum["params"]
+        sp = sed_func(**params)
+
+        return sp
+
+    def _get_source(self):
+        src_func = FLUX_DISTRO_dict[self.source["distro_name"]]
+        params = self.source["params"]
+        src = src_func(self._get_spectrum(),
+                       **params)
+        return src
+
+    def set_sky_conditions(self, airmass=1.5, moon_phase=0.5, pwv=10):
         """
         TODO: Check how ScopeSim can read this
-        :return:
+        TODO: turbulence and iq affect the PSF!
         """
 
-        self.sky = None
+        self.sky = dict(airmass=airmass, moon_phase=moon_phase, pwv=pwv)
+
+    def set_ao(self, distance, turbulence, iq):
+
+        self.ao_params = dict(disntance=distance, turbulence=turbulence, iq=iq)
+
+    def set_setup_obs(self, dit=60, ndit=1, filter_name="Ks"):
+
+        self.setup = dict(dit=dit, ndit=ndit, filter_name=filter_name)
+
+    def run(self, filename):
+        """
+        Runs the simulation and creates a report
+
+        Returns
+        -------
+        fits file with the simulation
+        report with the results
+
+        """
+
+        src = self._get_source()
+
+        micado = sim.OpticalTrain("MICADO")
+        micado.cmds["!OBS.filter_name"] = self.filter_name  # observing filter
+        micado.cmds["!INST.pixel_scale"] = self.pixel_size
+        micado["armazones_atmo_skycalc_ter_curve"].include = True
+        micado["armazones_atmo_default_ter_curve"].include = False
+        micado['detector_linearity'].include = False
+        micado.cmds["!OBS.dit"] = self.setup_obs["dit"]  # dit & ndit
+        micado.cmds["!OBS.ndit"] = self.setup_obs["ndit"]
+        micado.observe(src)
+        micado.cmds["!OBS.modes"] = ['SCAO', 'IMG_1.5mas']
+        hdus = micado.readout(filename=filename)
+
 
 
--- micado_etc/tests/test_etc.py
+++ micado_etc/tests/test_etc.py
@@ -8,6 +8,7 @@ from spextra import Spextrum
 
 from ..micado_etc import ETC
 
+
 class TestETCBasic:
 
     def test_initialization(self):
@@ -18,9 +19,21 @@ class TestETCBasic:
         etc = ETC(mode="imaging", pixel_size=0.0015, ao_mode="MCAO")
         assert etc.pixel_size == 0.0015
 
-    def test_select_sed(self):
+    def test_set_instrument(self):
+        etc = ETC(mode="spectroscopy", pixel_size=0.004, ao_mode="SCAO")
+        assert etc.mode =="spectroscopy"
+
+
+    def test_set_sed(self):
         etc = ETC()
-        etc.set_sed("template")
-        assert isinstance(etc.spectrum, Spextrum)
+        params = dict(template_name="kc96/s0", amplitude=18, filter_curve="R", redshift=1)
+        etc.set_sed("template", **params)
+        assert etc.spectrum["params"].keys() == params.keys()
+
+    def test_get_sed(self):
+        etc = ETC()
+        etc.set_sed("template", template_name="kc96/s0")
+        print(etc.spectrum)
+        assert isinstance(etc._get_spectrum(), Spextrum)
 
 
